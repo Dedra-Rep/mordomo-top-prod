11034cf7c52734ee86416fd0848de956f7a5ef18
Dedra-Rep <dedra.rep@gmail.com>
2025-12-27 09:21:43 -0300

    fix: vite build outDir to root dist

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,7 @@ export default defineConfig({
   root: "web",
   plugins: [react()],
   build: {
-    outDir: "../web-dist",
-    emptyOutDir: true,
-  },
+    outDir: "../dist",
+    emptyOutDir: true
+  }
 });
-
-
